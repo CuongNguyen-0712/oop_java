b2d23e91d604c0b6dcc1a9c4912fcb5135246398
Zair0u <quynhtien123123@gmail.com>
2024-11-20 11:59:02 +0700

    wait

--- Statistics/Statistics.java
+++ Statistics/Statistics.java
@@ -1,4 +1,11 @@
 package Statistics;
+import inputValue.inputScanner;
+import StaffManager.StaffManager;
+import BillManager.BillManager;
+import java.util.Vector;
 
 public class Statistics {
+    public static int (Vector<Book> listOfBook){
+
+    }
 }
