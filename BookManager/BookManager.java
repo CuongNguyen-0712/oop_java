a41cf1a395a33e0b37585ed525b9b4d3c11ba1f3
Zair0u <quynhtien123123@gmail.com>
2024-11-20 12:01:50 +0700

    wait

--- BookManager/BookManager.java
+++ BookManager/BookManager.java
@@ -21,6 +21,7 @@ public class BookManager
     {
         return listOfBook;
     }
+    
 
     public void add()
     {
--- Statistics/Statistics.java
+++ Statistics/Statistics.java
@@ -2,10 +2,11 @@ package Statistics;
 import inputValue.inputScanner;
 import StaffManager.StaffManager;
 import BillManager.BillManager;
+import BookManager.BookManager;
 import java.util.Vector;
 
 public class Statistics {
-    public static int (Vector<Book> listOfBook){
-
+    public static int bookStorage(Vector<Book> listOfBook){
+    
     }
 }
