a2cad9b80ea0026c0fac719e5e8ef018af264baf
Zair0u <quynhtien123123@gmail.com>
2024-11-21 21:46:50 +0700

    stastistics

--- Statistics/Statistics.java
+++ Statistics/Statistics.java
@@ -1,12 +1,28 @@
 package Statistics;
+
 import inputValue.inputScanner;
 import StaffManager.StaffManager;
+import BillManager.Bill;
 import BillManager.BillManager;
 import BookManager.BookManager;
 import java.util.Vector;
 
 public class Statistics {
-    public static int bookStorage(Vector<Book> listOfBook){
-    
+    public static void bookStorage(BookManager listOfBook){
+    System.out.println("Tong so luong sach con lai trong kho :"+listOfBook.totalBook());
+    listOfBook.countBookByCategory();
+    }
+    public static void StaffManage(StaffManager listOfStaff){
+    System.out.println("Tong so nhan vien la :"+listOfStaff.countStaff());
+    listOfStaff.countByJobStaff();
+    }
+    public static int totalRevenue(BillManager listOfBill){
+        return listOfBill.totalBill();
+    }
+    public static int benefit(StaffManager listOfStaff, BillManager listOfBill){
+        return total = listOfBill.totalBill()-listOfStaff.totalSalary();
+    }
+    public static bookSale(BillManager listOfBill){
+        
     }
 }
